c82477990b5eb3e8de263a89a7d8f5667ce0678d
Miles Conn <milesjconn@gmail.com>
2024-06-27 17:15:58 -0400

    Init commit got the hotkey working

--- main.ts
+++ main.ts
@@ -1,134 +1,132 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+const remote = require("electron").remote;
+const globalShortcut = remote.globalShortcut;
 
 interface MyPluginSettings {
-	mySetting: string;
+  hotkey: string;
+  miniDocumentSize: { width: number; height: number };
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+  // TODO: this
+  hotkey: "CommandOrControl+Shift+M",
+  miniDocumentSize: { width: 300, height: 200 },
+};
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    this.addCommand({
+      id: "open-mini-document",
+      name: "Open mini document",
+      callback: () => this.openMiniDocument(),
+    });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    this.registerGlobalHotkey();
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    this.addSettingTab(new MiniDocumentSettingTab(this.app, this));
+  }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+  onunload() {
+    this.deregisterGlobalHotkey();
+  }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+  registerGlobalHotkey() {
+    const ret = globalShortcut.register(this.settings.hotkey, () =>
+      this.openMiniDocument(),
+    );
 
-	onunload() {
+    if (!ret) {
+      console.error(
+        `Expected registering ${this.settings.hotkey} hotkey to work`,
+      );
+    }
+  }
 
-	}
+  deregisterGlobalHotkey() {
+    if (globalShortcut.isRegistered(this.settings.hotkey)) {
+      globalShortcut.unregister(this.settings.hotkey);
+    }
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  async openMiniDocument() {
+    const workspace = this.app.workspace;
+    const leaf = workspace.getLeaf("split");
+    const file = await this.app.vault.create(
+      `Mini Document ${Date.now()}.md`,
+      "",
+    );
+    await leaf.openFile(file, { state: { mode: "source" } });
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    // TODO:
+    // const { width, height } = this.settings.miniDocumentSize;
+    // leaf.setDimensions({ width, height });
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class MiniDocumentSettingTab extends PluginSettingTab {
+  plugin: MyPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+  display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+    new Setting(containerEl)
+      .setName("Hotkey")
+      .setDesc(
+        "Set the global hotkey for opening a mini document (e.g., CMD+SHIFT+M)",
+      )
+      .addText((text) =>
+        text
+          .setPlaceholder("CMD+SHIFT+M")
+          .setValue(this.plugin.settings.hotkey)
+          .onChange(async (value) => {
+            this.plugin.settings.hotkey = value;
+            await this.plugin.saveSettings();
+            this.plugin.registerGlobalHotkey();
+          }),
+      );
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    new Setting(containerEl)
+      .setName("Mini Document Width")
+      .setDesc("Set the width of the mini document (in pixels)")
+      .addText((text) =>
+        text
+          .setPlaceholder("300")
+          .setValue(String(this.plugin.settings.miniDocumentSize.width))
+          .onChange(async (value) => {
+            this.plugin.settings.miniDocumentSize.width = Number(value);
+            await this.plugin.saveSettings();
+          }),
+      );
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName("Mini Document Height")
+      .setDesc("Set the height of the mini document (in pixels)")
+      .addText((text) =>
+        text
+          .setPlaceholder("200")
+          .setValue(String(this.plugin.settings.miniDocumentSize.height))
+          .onChange(async (value) => {
+            this.plugin.settings.miniDocumentSize.height = Number(value);
+            await this.plugin.saveSettings();
+          }),
+      );
+  }
 }
